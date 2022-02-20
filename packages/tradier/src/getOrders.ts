eda17bb068a0fa6747e3080ccbf026f75405fed9
Mwein <mweinberg21@gmail.com>
2022-02-20 16:51:55 -0600

    Did some typescript

--- packages/tradier/src/getOrders.ts
+++ packages/tradier/src/getOrders.ts
@@ -1,8 +1,27 @@
 import * as network from './network'
 import { isOption, getType } from '@penny/option-symbol-parser'
 
+type OrderType = 'market' | 'limit' | 'stop' | 'stop_limit' | 'debit' | 'credit' | 'even'
+type OrderSide = 'buy' | 'buy_to_cover' | 'sell' | 'sell_short' | 'buy_to_open' | 'buy_to_close' | 'sell_to_open' | 'sell_to_close'
+type OrderStatus = 'open' | 'partially_filled' | 'filled' | 'expired' | 'canceled' | 'pending' | 'rejected' | 'error'
+type OrderDuration = 'day' | 'pre' | 'post' | 'gtc'
+type OrderClass = 'equity' | 'option' | 'combo' | 'multileg'
 
-export const filterForCoveredCallOrders = orders =>
+type Order = {
+  id: number,
+  symbol: string,
+  option_symbol?: string,
+  type: OrderType,
+  side: OrderSide,
+  quantity: number,
+  status: OrderStatus,
+  duration: OrderDuration,
+  price: number,
+  class: OrderClass,
+}
+
+
+export const filterForCoveredCallOrders = (orders: Order[]) : Order[] =>
   orders.filter(ord =>
     ord.class === 'option'
     && [ 'open', 'partially_filled', 'pending', 'calculated', 'accepted_for_bidding', 'held' ].includes(ord.status)
@@ -11,7 +30,7 @@ export const filterForCoveredCallOrders = orders =>
   )
 
 
-export const filterForCashSecuredPutOrders = orders =>
+export const filterForCashSecuredPutOrders = (orders: Order[]) : Order[] =>
   orders.filter(ord =>
     ord.class === 'option'
     && [ 'open', 'partially_filled', 'pending', 'calculated', 'accepted_for_bidding', 'held' ].includes(ord.status)
@@ -20,7 +39,7 @@ export const filterForCashSecuredPutOrders = orders =>
   )
 
 
-export const filterForOptionBuyToCloseOrders = orders =>
+export const filterForOptionBuyToCloseOrders = (orders: Order[]) : Order[] =>
   orders.filter(ord =>
     ord.class === 'option'
     && [ 'open', 'partially_filled', 'pending', 'calculated', 'accepted_for_bidding', 'held' ].includes(ord.status)
@@ -29,7 +48,7 @@ export const filterForOptionBuyToCloseOrders = orders =>
   )
 
 
-export const getOrder = async orderId => {
+export const getOrder = async (orderId: number) : Promise<Order> => {
   const url = `accounts/${process.env.ACCOUNTNUM}/orders/${orderId}`
   const response = await network.get(url)
   if (response.order === 'null') {
@@ -39,7 +58,7 @@ export const getOrder = async orderId => {
 }
 
 
-export const getOrders = async () => {
+export const getOrders = async () : Promise<Order[]> => {
   const url = `accounts/${process.env.ACCOUNTNUM}/orders`
   const response = await network.get(url)
   if (response.orders === 'null') {
