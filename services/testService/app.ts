d3ad896c92dfed5786e42b4c749b9ebb5b4bd89e
Mwein <mweinberg21@gmail.com>
2022-02-20 14:57:18 -0600

    Test service to check that db models work properly

--- services/testService/app.ts
+++ services/testService/app.ts
@@ -1,3 +1,32 @@
-import { isOption } from "@penny/option-symbol-parser"
+require('dotenv').config({ path: '../../.env' })
 
-console.log('isOption!!', isOption('AAPL'))
+import { incomeTargetModel, logModel } from '@penny/db-models'
+import * as mongoose from 'mongoose'
+
+
+const checkDB = async () => {
+  const incomeModels = await incomeTargetModel.find()
+  const logModels = await logModel.find()
+  console.log(incomeModels)
+  console.log(logModels)
+}
+
+
+// Recursively continuously try until the damn thing decides to work
+const connectToDB = () => {
+  console.log('Connecting to Database')
+
+  mongoose.connect(process.env.CONNECTION_STRING, null, err => {
+    if (err) {
+      // Not much choice in logging to a database we can't connect to
+      console.log('Database Connection Failure - Trying Again')
+
+      connectToDB()
+      return
+    }
+
+    checkDB()
+  })
+}
+
+connectToDB()
