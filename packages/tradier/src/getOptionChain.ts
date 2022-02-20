8d4f3c9a017655451ad067274ab8776618504fc6
Mwein <mweinberg21@gmail.com>
2022-02-20 17:30:07 -0600

    Added formatting and types to option chain func

--- packages/tradier/src/getOptionChain.ts
+++ packages/tradier/src/getOptionChain.ts
@@ -1,23 +1,50 @@
 import * as network from './network'
 
-type ChainLink = {
+type TradierChainLink = {
   symbol: string,
+  root_symbol: string,
   strike: number,
+  bid: number,
   option_type: 'put' | 'call',
+  expiration_date,
   greeks: {
     delta: number,
   }
 }
 
-export const getOptionChain = async (symbol: string, expiration: string) : Promise<ChainLink[]> => {
+type OptionChainLink = {
+  symbol: string,
+  underlying: string,
+  type: 'put' | 'call'
+  strike: number,
+  premium: number,
+  expiration: string,
+  delta: number
+}
+
+
+export const formatChain = (chain: TradierChainLink[]) : OptionChainLink[] => {
+  return chain.map(option => ({
+    symbol: option.symbol,
+    underlying: option.root_symbol,
+    type: option.option_type,
+    strike: option.strike,
+    premium: Number((option.bid * 100).toFixed()),
+    expiration: option.expiration_date,
+    delta: Math.abs(option.greeks.delta)
+  }))
+}
+
+
+export const getOptionChain = async (symbol: string, expiration: string) : Promise<OptionChainLink[]> => {
   const url = `markets/options/chains?symbol=${symbol}&expiration=${expiration}&greeks=true`
   const response = await network.get(url)
   if (response.options === 'null') {
     return []
   }
   if (Array.isArray(response.options.option)) {
-    return []
+    return formatChain(response.options.option)
   } else {
-    return [ response.options.option ]
+    return formatChain([ response.options.option ])
   }
 }
