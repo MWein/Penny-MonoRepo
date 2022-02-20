4608198731260b6b17660770cc6c07b2439bc696
Mwein <mweinberg21@gmail.com>
2022-02-20 15:40:17 -0600

    Logger package

---
+++ packages/logger/index.test.ts
@@ -0,0 +1,183 @@
+import { logModel } from '@penny/db-models'
+jest.mock('@penny/db-models')
+
+import {
+  _logWithObject,
+  _logWithMessage,
+  log,
+  getLogs,
+  clearOldLogs
+} from './index'
+
+
+describe('_logWithObject', () => {
+  const originalConsoleLog = console.log
+  let saveFunc
+
+  beforeEach(() => {
+    saveFunc = jest.fn()
+    logModel.mockReturnValue({
+      save: saveFunc,
+    })
+    console.log = jest.fn()
+  })
+
+  afterEach(() => {
+    console.log = originalConsoleLog
+  })
+
+  it('On failure, console logs', async () => {
+    logModel.mockImplementation(() => {
+      throw new Error('Oh no!!!!!!')
+    })
+    await _logWithObject({ message: 'something' })
+    expect(logModel).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('Error reaching database')
+  })
+
+  it('On success, saves and console logs the message', async () => {
+    await _logWithObject({ type: 'error', message: 'something' })
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logModel).toHaveBeenCalledWith({ type: 'error', message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('error', ':', 'something')
+  })
+
+  it('Does not console log if type is \'ping\'', async () => {
+    await _logWithObject({ type: 'ping', message: 'something' })
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logModel).toHaveBeenCalledWith({ type: 'ping', message: 'something' })
+    expect(console.log).not.toHaveBeenCalled()
+  })
+
+  it('Console log says \'info\' if type is not provided', async () => {
+    await _logWithObject({ message: 'something' })
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logModel).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('info', ':', 'something')
+  })
+})
+
+
+describe('_logWithMessage', () => {
+  const originalConsoleLog = console.log
+  let saveFunc
+
+  beforeEach(() => {
+    saveFunc = jest.fn()
+    logModel.mockReturnValue({
+      save: saveFunc,
+    })
+    console.log = jest.fn()
+  })
+
+  afterEach(() => {
+    console.log = originalConsoleLog
+  })
+
+  it('On failure, console logs', async () => {
+    logModel.mockImplementation(() => {
+      throw new Error('Oh no!!!!!!')
+    })
+    await _logWithMessage('something')
+    expect(logModel).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('Error reaching database')
+  })
+
+  it('On success, saves and console logs the message', async () => {
+    await _logWithMessage('something')
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logModel).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('info', ':', 'something')
+  })
+})
+
+
+describe('log', () => {
+  const originalConsoleLog = console.log
+  let saveFunc
+
+  beforeEach(() => {
+    saveFunc = jest.fn()
+    logModel.mockReturnValue({
+      save: saveFunc,
+    })
+    console.log = jest.fn()
+  })
+
+  afterEach(() => {
+    console.log = originalConsoleLog
+  })
+
+  it('Calls correct function if given an object', async () => {
+    await log({ type: 'info', message: 'something' })
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logModel).toHaveBeenCalledWith({ type: 'info', message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('info', ':', 'something')
+  })
+
+  it('Calls correct function if given a string', async () => {
+    await log('something')
+    expect(saveFunc).toHaveBeenCalledTimes(1)
+    expect(logModel).toHaveBeenCalledWith({ message: 'something' })
+    expect(console.log).toHaveBeenCalledWith('info', ':', 'something')
+  })
+})
+
+
+describe('getLogs', () => {
+  it('Gets the logs', async () => {
+    const select = jest.fn()
+    const sort = jest.fn().mockReturnValue({
+      select,
+    })
+    logModel.find = jest.fn().mockReturnValue({
+      sort,
+    })
+
+    select.mockReturnValue([
+      'some',
+      'logs'
+    ])
+
+    const result = await getLogs()
+    expect(result).toEqual([
+      'some',
+      'logs'
+    ])
+  })
+})
+
+
+describe('clearOldLogs', () => {
+  const originalConsoleLog = console.log
+
+  beforeEach(() => {
+    console.log = jest.fn()
+    logModel.deleteMany = jest.fn()
+  })
+
+  afterEach(() => {
+    console.log = originalConsoleLog
+    jest.useRealTimers()
+  })
+
+  it('Console logs on failure', async () => {
+    (logModel.deleteMany as unknown as jest.Mock).mockImplementation(() => {
+      throw new Error('Shit')
+    })
+    await clearOldLogs()
+    expect(console.log).toHaveBeenCalledWith('Error reaching database')
+  })
+
+  it('Calls clear logs query 90 days ago', async () => {
+    jest.useFakeTimers().setSystemTime(new Date('2019-05-29').getTime())
+    await clearOldLogs()
+    expect(logModel.deleteMany).toHaveBeenCalledWith({ date: { $lte: 1551315600000}})
+  })
+
+  it('Calls clear logs query 90 days ago - again', async () => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-10-12').getTime())
+    await clearOldLogs()
+    expect(logModel.deleteMany).toHaveBeenCalledWith({ date: { $lte: 1626220800000}})
+  })
+})
---
+++ packages/logger/index.ts
@@ -0,0 +1,63 @@
+import { logModel } from '@penny/db-models'
+
+type LogType = 'info' | 'ping' | 'error'
+
+type LogObject = {
+  type?: LogType,
+  message: string
+}
+
+
+// Log data is the object that will go directly to Mongo
+export const _logWithObject = async (logData: LogObject) => {
+  try {
+    const newLog = new logModel(logData)
+    await newLog.save()
+    if (logData.type !== 'ping') {
+      console.log(logData.type || 'info', ':', logData.message)
+    }
+  } catch (e) {
+    console.log('Error reaching database')
+  }
+}
+
+
+export const _logWithMessage = async (message: string) => {
+  try {
+    const newLog = new logModel({
+      message,
+    })
+    await newLog.save()
+    console.log('info', ':', message)
+  } catch (e) {
+    console.log('Error reaching database')
+  }
+}
+
+
+export const log = async (logData : string | LogObject) => {
+  if (typeof logData === 'string') {
+    await _logWithMessage(logData)
+  }
+  if (typeof logData === 'object') {
+    await _logWithObject(logData)
+  }
+}
+
+
+export const getLogs = async () : Promise<String[]> => {
+  const logs = await logModel.find().sort({ date: -1 }).select('-_id -__v')
+  return logs
+}
+
+
+export const clearOldLogs = async () => {
+  try {
+    const DELETEOLDERTHANDAYS = 90
+    const today = new Date()
+    const priorDate = new Date().setDate(today.getDate() - DELETEOLDERTHANDAYS)
+    await logModel.deleteMany({ date: { $lte: priorDate } })
+  } catch (e) {
+    console.log('Error reaching database')
+  }
+}
