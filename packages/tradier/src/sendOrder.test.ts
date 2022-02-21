12184847f4ea04f82f0101a07c63ffafed1f538b
Mwein <mweinberg21@gmail.com>
2022-02-20 21:36:04 -0600

    More test refactoring

--- packages/tradier/src/sendOrder.test.ts
+++ packages/tradier/src/sendOrder.test.ts
@@ -10,7 +10,7 @@ import {
 } from './sendOrder'
 
 
-const correctUrlAndBodyTest = async (func: Function, funcArgs: Array<any>, expectBody) => {
+const correctUrlAndBodyTest = async (func: Function, funcArgs: Array<any>, expectBody: object) => {
   process.env.ACCOUNTNUM = 'thisisanaccountnumber'
     // @ts-ignore
     network.post.mockReturnValue({ status: 'ok' })
@@ -22,7 +22,6 @@ const correctUrlAndBodyTest = async (func: Function, funcArgs: Array<any>, expec
     // @ts-ignore
     expect(network.post.mock.calls[0][2]).toEqual(false)
 }
-
 
 const testForFailure = async (func: Function, funcArgs: Array<any>, failureMessage: string) => {
   // @ts-ignore
@@ -45,159 +44,91 @@ const testForHappyPathReturn = async (func: Function, funcArgs: Array<any>, succ
 }
 
 
-describe('buy', () => {
-  beforeEach(() => {
-    // @ts-ignore
-    network.post = jest.fn()
-    // @ts-ignore
-    logUtil.log = jest.fn()
-  })
+const runTests = async (func: Function, funcArgs: Array<any>, successMessage: string, failureMessage: string, expectBody: object) => {
+  // @ts-ignore
+  network.post = jest.fn()
+  // @ts-ignore
+  logUtil.log = jest.fn()
+  await correctUrlAndBodyTest(func, funcArgs, expectBody)
+  jest.resetAllMocks()
+  await testForFailure(func, funcArgs, failureMessage)
+  jest.resetAllMocks()
+  await testForHappyPathReturn(func, funcArgs, successMessage)
+}
 
-  it('Calls with the correct url and body; skips throttle', async () => {
-    await correctUrlAndBodyTest(buy, [ 'AAPL', 2 ], {
+
+describe('sendOrder common functions', () => {
+  it('buy', async () => {
+    const quantity = Math.floor(Math.random() * 100) + 1
+    await runTests(buy, [ 'AAPL', quantity ], `Buy ${quantity} AAPL`, `Buy ${quantity} AAPL Failed`, {
       account_id: 'thisisanaccountnumber',
       class: 'equity',
       symbol: 'AAPL',
       side: 'buy',
-      quantity: 2,
+      quantity,
       type: 'market',
       duration: 'day',
     })
   })
 
-  it('Returns failed status object if network call throws', async () => {
-    await testForFailure(buy, [ 'AAPL', 1 ], 'Buy 1 AAPL Failed')
-  })
-
-  it('On success, returns whatever the endpoint returned', async () => {
-    await testForHappyPathReturn(buy, [ 'AAPL', 1 ], 'Buy 1 AAPL')
-  })
-})
-
-
-describe('sellToOpen', () => {
-  beforeEach(() => {
-    // @ts-ignore
-    network.post = jest.fn()
-    // @ts-ignore
-    logUtil.log = jest.fn()
-  })
-
-  it('Calls with the correct url and body; skips throttle', async () => {
-    await correctUrlAndBodyTest(sellToOpen, [ 'AAPL', 'AAAAAAPL', 2 ], {
+  it('sellToOpen', async () => {
+    const quantity = Math.floor(Math.random() * 100) + 1
+    await runTests(sellToOpen, [ 'AAPL', 'AAAAAAPL', quantity ], `Sell-to-open ${quantity} AAAAAAPL`, `Sell-to-open ${quantity} AAAAAAPL Failed`, {
       account_id: 'thisisanaccountnumber',
       class: 'option',
       symbol: 'AAPL',
       option_symbol: 'AAAAAAPL',
       side: 'sell_to_open',
-      quantity: 2,
+      quantity,
       type: 'market',
       duration: 'day',
     })
   })
 
-  it('Returns failed status object if network call throws', async () => {
-    await testForFailure(sellToOpen, [ 'AAPL', 'AAAAAPL', 1 ], 'Sell-to-open 1 AAAAAPL Failed')
-  })
-
-  it('On success, returns whatever the endpoint returned', async () => {
-    await testForHappyPathReturn(sellToOpen, [ 'AAPL', 'AAAAAPL', 1 ], 'Sell-to-open 1 AAAAAPL')
-  })
-})
-
-
-describe('buyToClose', () => {
-  beforeEach(() => {
-    // @ts-ignore
-    network.post = jest.fn()
-    // @ts-ignore
-    logUtil.log = jest.fn()
-  })
-
-  it('Calls with the correct url and body; skips throttle', async () => {
-    await correctUrlAndBodyTest(buyToClose, [ 'AAPL', 'AAAAAAPL', 2, 0.18 ], {
+  it('buyToClose', async () => {
+    const quantity = Math.floor(Math.random() * 100) + 1
+    await runTests(buyToClose, [ 'AAPL', 'AAAAAAPL', quantity, 0.18 ], `Buy-to-close ${quantity} AAAAAAPL`, `Buy-to-close ${quantity} AAAAAAPL Failed`, {
       account_id: 'thisisanaccountnumber',
       class: 'option',
       symbol: 'AAPL',
       option_symbol: 'AAAAAAPL',
       side: 'buy_to_close',
-      quantity: 2,
+      quantity,
       type: 'limit',
       price: 0.18,
       duration: 'gtc',
     })
   })
 
-  it('Returns failed status object if network call throws', async () => {
-    await testForFailure(buyToClose, [ 'AAPL', 'AAAAAPL', 1, 0.11 ], 'Buy-to-close 1 AAAAAPL Failed')
-  })
-
-  it('On success, returns whatever the endpoint returned', async () => {
-    await testForHappyPathReturn(buyToClose, [ 'AAPL', 'AAAAAPL', 1, 0.10 ], 'Buy-to-close 1 AAAAAPL')
-  })
-})
-
-
-describe('sellToClose', () => {
-  beforeEach(() => {
-    // @ts-ignore
-    network.post = jest.fn()
-    // @ts-ignore
-    logUtil.log = jest.fn()
-  })
-
-  it('Calls with the correct url and body; skips throttle', async () => {
-    await correctUrlAndBodyTest(sellToClose, [ 'AAPL', 'AAAAAAPL', 2 ], {
+  it('sellToClose', async () => {
+    const quantity = Math.floor(Math.random() * 100) + 1
+    await runTests(sellToClose, [ 'AAPL', 'AAAAAAPL', quantity ], `Sell-to-close ${quantity} AAAAAAPL`, `Sell-to-close ${quantity} AAAAAAPL Failed`, {
       account_id: 'thisisanaccountnumber',
       class: 'option',
       symbol: 'AAPL',
       option_symbol: 'AAAAAAPL',
       side: 'sell_to_close',
-      quantity: 2,
+      quantity,
       type: 'market',
       duration: 'gtc',
     })
   })
 
-  it('Returns failed status object if network call throws', async () => {
-    await testForFailure(sellToClose, [ 'AAPL', 'AAAAAPL', 1 ], 'Sell-to-close 1 AAAAAPL Failed')
-  })
-
-  it('On success, returns whatever the endpoint returned', async () => {
-    await testForHappyPathReturn(sellToClose, [ 'AAPL', 'AAAAAPL', 1 ], 'Sell-to-close 1 AAAAAPL')
-  })
-})
-
-
-describe('buyToCloseMarket', () => {
-  beforeEach(() => {
-    // @ts-ignore
-    network.post = jest.fn()
-    // @ts-ignore
-    logUtil.log = jest.fn()
-  })
-
-  it('Calls with the correct url and body; skips throttle', async () => {
-    await correctUrlAndBodyTest(buyToCloseMarket, [ 'AAPL', 'AAAAAAPL', 2 ], {
+  it('buyToCloseMarket', async () => {
+    const quantity = Math.floor(Math.random() * 100) + 1
+    await runTests(buyToCloseMarket, [ 'AAPL', 'AAAAAAPL', quantity ], `Buy-to-close Market Price ${quantity} AAAAAAPL`, `Buy-to-close Market Price ${quantity} AAAAAAPL Failed`, {
       account_id: 'thisisanaccountnumber',
       class: 'option',
       symbol: 'AAPL',
       option_symbol: 'AAAAAAPL',
       side: 'buy_to_close',
-      quantity: 2,
+      quantity,
       type: 'market',
       duration: 'gtc',
     })
   })
-
-  it('Returns failed status object if network call throws', async () => {
-    await testForFailure(buyToCloseMarket, [ 'AAPL', 'AAAAAPL', 1 ], 'Buy-to-close Market Price 1 AAAAAPL Failed')
-  })
-
-  it('On success, returns whatever the endpoint returned', async () => {
-    await testForHappyPathReturn(buyToCloseMarket, [ 'AAPL', 'AAAAAPL', 1 ], 'Buy-to-close Market Price 1 AAAAAPL')
-  })
 })
+
 
 
 describe('cancelOrders', () => {
