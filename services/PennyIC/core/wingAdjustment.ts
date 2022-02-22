7995dce749e3a449669d924533e873e9f40c78c0
Mwein <mweinberg21@gmail.com>
2022-02-21 23:36:31 -0600

    Finally got to do a promise.all =D

--- services/PennyIC/core/wingAdjustment.ts
+++ services/PennyIC/core/wingAdjustment.ts
@@ -2,6 +2,8 @@ import * as tradier from '@penny/tradier'
 import { getUnderlying, getStrike, getType } from '@penny/option-symbol-parser'
 import { uniq } from 'lodash'
 
+import { MultilegOptionLeg } from '@penny/tradier'
+
 
 // Returns the distance between the short strike and the current price
 const getDistanceFromPrice = (spread: { short: string, long: string }, priceMap: object) => {
@@ -20,6 +22,8 @@ export const wingAdjustment = async () => {
   const spreadGroupResults = tradier.groupOptionsIntoSpreads(positions)
   const allSpreads = [ ...spreadGroupResults.call.spreads, ...spreadGroupResults.put.spreads ]
 
+  // TODO Filter out ones that expire today
+
   const spreadTickers = uniq(allSpreads.map(spread => getUnderlying(spread.short)))
   const prices = await tradier.getPrices(spreadTickers)
   const priceMap = prices.reduce((acc, price) => ({ ...acc, [price.symbol]: price.price }), {})
@@ -35,14 +39,36 @@ export const wingAdjustment = async () => {
   // So the closes are sent ASAP. Opening the new positions will take awhile
 
   // Send close orders
-  for (let x = 0; x < spreadsToClose.length; x++) {
+  await Promise.all(spreadsToClose.map(async spread => {
+    const underlying = getUnderlying(spread.short)
+    const legs: MultilegOptionLeg[] = [
+      {
+        symbol: spread.short,
+        side: 'buy_to_close',
+        quantity: 1
+      },
+      {
+        symbol: spread.long,
+        side: 'sell_to_close',
+        quantity: 1
+      },
+    ]
+    //await tradier.multilegOptionOrder(underlying, 'market', legs)
+  }))
 
 
-  }
+  const newWingsToOpen = spreadsToClose.map(spread => ({
+    type: getType(spread.short),
+    symbol: getUnderlying(spread.short)
+  }))
+
+  console.log(newWingsToOpen)
 
   // Open new positions
-  for (let x = 0; x < spreadsToClose.length; x++) {
+  // for (let x = 0; x < spreadsToClose.length; x++) {
+  //   const spreadToClose = spreadsToClose[x]
 
-  }
+
+  // }
 
 }
