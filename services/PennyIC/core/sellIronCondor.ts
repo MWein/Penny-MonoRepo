8a593a0abaf8a717019f1e43cb51165092aa752b
Mwein <mweinberg21@gmail.com>
2022-02-21 21:24:03 -0600

    Try-catches, isMarketOpen, position filtering

--- services/PennyIC/core/sellIronCondor.ts
+++ services/PennyIC/core/sellIronCondor.ts
@@ -1,6 +1,7 @@
 import * as tradier from '@penny/tradier'
+import { isOption, getUnderlying } from '@penny/option-symbol-parser'
 import { OptionChainLink, MultilegOptionLeg } from '@penny/tradier'
-
+import { uniq } from 'lodash'
 
 
 type ChainLinkWithDeltaDist = OptionChainLink & {
@@ -21,75 +22,85 @@ const _selectLinkClosestToTarget = (chain: ChainLinkWithDeltaDist[]): ChainLinkW
 
 
 export const sellSpread = async (chain: OptionChainLink[], symbol: string, type: 'call' | 'put', shortDelta: number, targetStrikeWidth: number): Promise<void> => {
-  const typeChain = chain.filter(x => x.type === type)
-  const typeChainWithDist = _insertDistanceToTargetDelta(typeChain, shortDelta)
-  const shortLink = _selectLinkClosestToTarget(typeChainWithDist)
-
-  // Different filter params depending on the type
-  const filterFunc = type === 'put' ?
-    (link: OptionChainLink) => (shortLink.strike - link.strike) <= targetStrikeWidth && link.strike < shortLink.strike
-    : (link: OptionChainLink) => (link.strike - shortLink.strike) <= targetStrikeWidth && link.strike > shortLink.strike
-
-  // After non-candidates for long is filtered out, this will modify the array (or not)
-  // The selection will be the strike with the largest distance from the short strike
-  // For calls, the chain has to be reversed to make the selection
-  const modifier = type === 'put' ?
-    (chain: OptionChainLink[]) => chain :
-    (chain: OptionChainLink[]) => chain.reverse()
-
-  const longLinkOptions = modifier(typeChainWithDist.filter(filterFunc))
-
-  if (longLinkOptions.length === 0) {
-    return
-  }
-
-  const longLink = longLinkOptions[0]
-
-  const legs: MultilegOptionLeg[] = [
-    {
-      symbol: shortLink.symbol,
-      side: 'sell_to_open',
-      quantity: 1
-    },
-    {
-      symbol: longLink.symbol,
-      side: 'buy_to_open',
-      quantity: 1
+  try {
+    const typeChain = chain.filter(x => x.type === type)
+    const typeChainWithDist = _insertDistanceToTargetDelta(typeChain, shortDelta)
+    const shortLink = _selectLinkClosestToTarget(typeChainWithDist)
+  
+    // Different filter params depending on the type
+    const filterFunc = type === 'put' ?
+      (link: OptionChainLink) => (shortLink.strike - link.strike) <= targetStrikeWidth && link.strike < shortLink.strike
+      : (link: OptionChainLink) => (link.strike - shortLink.strike) <= targetStrikeWidth && link.strike > shortLink.strike
+  
+    // After non-candidates for long is filtered out, this will modify the array (or not)
+    // The selection will be the strike with the largest distance from the short strike
+    // For calls, the chain has to be reversed to make the selection
+    const modifier = type === 'put' ?
+      (chain: OptionChainLink[]) => chain :
+      (chain: OptionChainLink[]) => chain.reverse()
+  
+    const longLinkOptions = modifier(typeChainWithDist.filter(filterFunc))
+  
+    if (longLinkOptions.length === 0) {
+      return
     }
-  ]
-
-  // SELL SELL SELL
-  await tradier.multilegOptionOrder(symbol, 'credit', legs)
+  
+    const longLink = longLinkOptions[0]
+  
+    const legs: MultilegOptionLeg[] = [
+      {
+        symbol: shortLink.symbol,
+        side: 'sell_to_open',
+        quantity: 1
+      },
+      {
+        symbol: longLink.symbol,
+        side: 'buy_to_open',
+        quantity: 1
+      }
+    ]
+  
+    // SELL SELL SELL
+    await tradier.multilegOptionOrder(symbol, 'credit', legs)
+  } catch (e) {
+    // TODO Log error
+  }
 }
 
 
 
 
 export const sellIronCondor = async (symbol: string, shortDelta: number, targetStrikeWidth: number) => {
-  const expiratons = await tradier.getExpirations(symbol)
-  if (!expiratons || expiratons.length === 0) {
-    return
+  try {
+    const expiratons = await tradier.getExpirations(symbol)
+    if (!expiratons || expiratons.length === 0) {
+      return
+    }
+    const expiration = expiratons[0]
+    const chain = await tradier.getOptionChain(symbol, expiration)
+  
+    await sellSpread(chain, symbol, 'put', shortDelta, targetStrikeWidth)
+    await sellSpread(chain, symbol, 'call', shortDelta, targetStrikeWidth)
+  } catch (e) {
+    // TODO Log error
   }
-  const expiration = expiratons[0]
-  const chain = await tradier.getOptionChain(symbol, expiration)
-
-  await sellSpread(chain, symbol, 'put', shortDelta, targetStrikeWidth)
-  await sellSpread(chain, symbol, 'call', shortDelta, targetStrikeWidth)
 }
 
 
 
 
-// SPY, IWM, QQQ version
-
 export const sellIronCondorBigThree = async () => {
   const bigThree = [ 'SPY', 'IWM', 'QQQ' ]
 
-  // Is bigThree enabled?
+  // TODO Is bigThree enabled?
 
-  // Is market open
+  // Is market open?
+  const isOpen = await tradier.isMarketOpen()
+  if (!isOpen) {
+    return
+  }
 
-  // Get big three settings (individual)
+  // TODO Get big three settings (individual)
     // Enabled?
     // Short delta
     // maxStrikeWidth
@@ -102,21 +113,29 @@ export const sellIronCondorBigThree = async () => {
 
 
 export const sellIronCondors = async () => {
-  // Is iron condor enabled?
+  // TODO Is iron condor enabled?
 
   // Is market open?
+  const isOpen = await tradier.isMarketOpen()
+  if (!isOpen) {
+    return
+  }
 
   // TODO get stocks from DB
-  const symbols = []
+  const symbols = [ 'AAPL', 'TSLA', 'AAL' ]
 
   // TODO Filter for enabled
     // Map out just symbols
 
-  // TODO Filter out ones where positions already exist
 
-  for (let x = 0; x < symbols.length; x++) {
-    console.log(symbols[x])
-    await sellIronCondor(symbols[x], 0.1, 1)
+  //  Filter out ones where positions already exist
+  const positions = await tradier.getPositions()
+  const optionPositions = positions.filter(pos => isOption(pos.symbol))
+  const openSymbols = uniq(optionPositions.map(pos => getUnderlying(pos.symbol)))
+  const symbolsWithoutPositions = symbols.filter(symbol => !openSymbols.includes(symbol))
+
+  for (let x = 0; x < symbolsWithoutPositions.length; x++) {
+    console.log(symbolsWithoutPositions[x])
+    await sellIronCondor(symbolsWithoutPositions[x], 0.1, 1)
   }
-
 }
