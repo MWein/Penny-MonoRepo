2c476253e475c435035a0eced5e96ce6c7704b51
Mike <mweinberg21@gmail.com>
2022-04-01 02:32:18 -0500

    Added material ui

--- services/PennyUI/src/App.tsx
+++ services/PennyUI/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Paper from '@mui/material/Paper'
 
 const App = () => {
   return (
-    <div>
+    <Paper>
       Hello World
-    </div>
+    </Paper>
   )
 }
 
