f6f44ec18deed7789d34b7e281cdee8d5bce4d0b
Mike <mweinberg21@gmail.com>
2022-04-02 15:29:00 -0500

    Added default dates to gain loss function

--- packages/tradier/src/getGainLoss.ts
+++ packages/tradier/src/getGainLoss.ts
@@ -12,8 +12,13 @@ export type GainLossDetails = {
 }
 
 
-export const getGainLoss = async (pageNum: number = 1, limit: number = 1000) : Promise<GainLossDetails[]> => {
-  const url = `accounts/${process.env.ACCOUNTNUM}/gainloss?page=${pageNum}&limit=${limit}`
+export const getGainLoss = async (
+  pageNum: number = 1,
+  limit: number = 1000,
+  start: string = '2020-01-01',
+  end: string = new Date().toISOString().split('T')[0],
+) : Promise<GainLossDetails[]> => {
+  const url = `accounts/${process.env.ACCOUNTNUM}/gainloss?page=${pageNum}&limit=${limit}&start=${start}&end=${end}`
   const response = await network.get(url)
   if (response.gainloss === 'null') {
     return []
