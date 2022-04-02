70ce0cf2cd81e8a1a89e412a62796a597eb7d19d
Mike <mweinberg21@gmail.com>
2022-04-02 14:35:22 -0500

    Updated penny status endpoint to return true or false

--- services/Penny-Data/controllers/pennyStatus.ts
+++ services/Penny-Data/controllers/pennyStatus.ts
@@ -3,10 +3,12 @@ import * as pennyStatusUtil from '../services/pennyStatus'
 
 const pennyStatusController = async (req, res) : Promise<void> => {
   try {
-    const lastLogDate = await pennyStatusUtil.getLastLogDate()
+    const lastLogDate: string = await pennyStatusUtil.getLastLogDate()
+    const timeSince = new Date().getUTCMilliseconds() - new Date(lastLogDate).getUTCMilliseconds()
+    const healthy = timeSince < 3600020 // 1 hour and 20ms
 
     res.json({
-      lastLogDate
+      healthy
     })
   } catch (e) {
     res.status(500).send('Error')
