8b4392574f84983462e46464a68a624a62138042
Mike <mweinberg21@gmail.com>
2022-04-02 00:22:07 -0500

    Environment ribbon

---
+++ services/PennyUI/src/components/EnvironmentRibbon.tsx
@@ -0,0 +1,16 @@
+import Typography from '@mui/material/Typography'
+
+const EnvironmentRibbon = () => (
+  <Typography variant='h6' style={{
+    textAlign: 'center',
+    background: 'lightBlue',
+    height: '35px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }}>
+    NonProd
+  </Typography>
+)
+
+export default EnvironmentRibbon
