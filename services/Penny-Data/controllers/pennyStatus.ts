84ee8a916dd39b0302c169f3fc19dedac59a1460
Mike <mweinberg21@gmail.com>
2022-04-02 18:19:17 -0500

    Fixed tests. Also I'm an idiot

--- services/Penny-Data/controllers/pennyStatus.ts
+++ services/Penny-Data/controllers/pennyStatus.ts
@@ -4,7 +4,7 @@ import * as pennyStatusUtil from '../services/pennyStatus'
 const pennyStatusController = async (req, res) : Promise<void> => {
   try {
     const lastLogDate: string = await pennyStatusUtil.getLastLogDate()
-    const timeSince = new Date().getUTCMilliseconds() - new Date(lastLogDate).getUTCMilliseconds()
+    const timeSince = new Date().valueOf() - new Date(lastLogDate).valueOf()
     const healthy = timeSince < 3600020 // 1 hour and 20ms
 
     res.json({
