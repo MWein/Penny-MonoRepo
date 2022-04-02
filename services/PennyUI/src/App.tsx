4b3cee9c827a9fb0ffe8e092f1146d6a09be47cd
Mike <mweinberg21@gmail.com>
2022-04-01 20:37:39 -0500

    Graph SVG

--- services/PennyUI/src/App.tsx
+++ services/PennyUI/src/App.tsx
@@ -3,13 +3,13 @@ import PositionChit from './components/PositionChit'
 const App = () => {
   return (
     <div>
-      <PositionChit ticker='TQQQ' gainLoss={40} maxGain={50} maxLoss={-7} hasCall hasPut />
-      <PositionChit ticker='SPY' gainLoss={40} maxGain={50} maxLoss={-7} hasCall />
-      <PositionChit ticker='IWM' gainLoss={40} maxGain={50} maxLoss={-7} hasPut />
+      <PositionChit ticker='TQQQ' gainLoss={70} maxGain={70} maxLoss={-30} hasCall hasPut />
+      <PositionChit ticker='SPY' gainLoss={40} maxGain={100} maxLoss={-8} hasCall />
+      <PositionChit ticker='IWM' gainLoss={20} maxGain={40} maxLoss={-15} hasPut />
       <br />
-      <PositionChit ticker='SPY' gainLoss={40} maxGain={50} maxLoss={-7} hasPut />
-      <PositionChit ticker='TQQQ' gainLoss={40} maxGain={50} maxLoss={-7} hasCall />
-      <PositionChit ticker='IWM' gainLoss={40} maxGain={50} maxLoss={-7} hasPut />
+      <PositionChit ticker='SPY' gainLoss={-2} maxGain={50} maxLoss={-20} hasPut />
+      <PositionChit ticker='TQQQ' gainLoss={5} maxGain={50} maxLoss={-100} hasCall />
+      <PositionChit ticker='IWM' gainLoss={-17} maxGain={50} maxLoss={-7} hasPut />
     </div>
   )
 }
--- services/PennyUI/src/components/PositionChit.tsx
+++ services/PennyUI/src/components/PositionChit.tsx
@@ -26,14 +26,58 @@ const PositionChit = ({
     </Typography>
   )
 
+  // Graph position settings
+  const graphStartPos = 10
+  const graphEndPos = 90
+  const maxGainLossHeight = 45
+
+  // Color settings
+  const baseGraphColor = 'black'
+
+  const gainLossTickColor = gainLoss > 0 ? 'green' : 'red'
+
+  // Calculated positions
+  const zeroPosition = 25
+  const gainLossPosition = 55
+
 
   return (
-    <Paper style={{ padding: '10px', width: '200px', height: '40px', display: 'inline-flex' }}>
+    <Paper className='position-chit'>
       <Typography variant='h6' className='horizontal-center'>
         {ticker}
       </Typography>
 
       <div style={{ flex: 1 }} />
+
+      <svg xmlns="http://www.w3.org/2000/svg" height='50px' width='100px' viewBox="0 0 100 50">
+        <g>
+          {/* Base Graph */}
+          <path d={`M ${graphStartPos} 25 L ${graphEndPos} 25`} stroke={baseGraphColor} />
+          <path d={`M ${graphStartPos} 15 L ${graphStartPos} 30`} stroke={baseGraphColor} />
+          <path d={`M ${graphEndPos} 15 L ${graphEndPos} 30`} stroke={baseGraphColor} />
+
+          {/* Zero Tick */}
+          <path d={`M ${zeroPosition} 20 L ${zeroPosition} 25`} stroke='black' />
+
+          {/* Gain Loss Tick */}
+          <path d={`M ${gainLossPosition} 20 L ${gainLossPosition} 25`} stroke={gainLossTickColor} strokeWidth={2} />
+
+          {/* Max Loss */}
+          <text text-anchor="middle" x={`${graphStartPos}`} y={`${maxGainLossHeight}`} style={{ fontSize: '12px' }}>{maxLoss}</text>
+
+          {/* Zero */}
+          <text text-anchor="middle" x={zeroPosition} y="35" style={{ fontSize: '12px' }}>0</text>
+
+          {/* Max Gain */}
+          <text text-anchor="middle" x={`${graphEndPos}`} y={`${maxGainLossHeight}`} style={{ fontSize: '12px' }}>{maxGain}</text>
+
+          {/* Gain Loss */}
+          <text text-anchor="middle" x={gainLossPosition} y="15" style={{ fontSize: '12px' }} stroke={gainLossTickColor}>{gainLoss}</text>
+        </g>
+      </svg>
+
+
+      <div style={{ width: '10px' }} />
 
       <div className='horizontal-center'>
         {callPutText(hasCall, 'Call')}
