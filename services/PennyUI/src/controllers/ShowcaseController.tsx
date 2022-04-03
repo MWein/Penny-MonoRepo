830dcdc64aab2e713b372c8d8d04f8af0a670066
Mike <mweinberg21@gmail.com>
2022-04-03 00:07:40 -0500

    Commented out mock data

--- services/PennyUI/src/controllers/ShowcaseController.tsx
+++ services/PennyUI/src/controllers/ShowcaseController.tsx
@@ -7,61 +7,61 @@ import fetchShowcaseData from '../network/showcase'
 
 // ****************** MOCK DATA *********************
 
-const generateMockPosition = (
-  ticker: string,
-  gainLoss: number,
-  maxLoss: number,
-  maxGain: number,
-  hasPut: boolean,
-  hasCall: boolean,
-) => ({
-  ticker,
-  gainLoss,
-  maxLoss,
-  maxGain,
-  hasPut,
-  hasCall,
-})
+// const generateMockPosition = (
+//   ticker: string,
+//   gainLoss: number,
+//   maxLoss: number,
+//   maxGain: number,
+//   hasPut: boolean,
+//   hasCall: boolean,
+// ) => ({
+//   ticker,
+//   gainLoss,
+//   maxLoss,
+//   maxGain,
+//   hasPut,
+//   hasCall,
+// })
 
-const randomMockPosition = (ticker: string) => {
-  const hasPut = Math.random().toFixed(0) === '1'
-  const hasCall = hasPut ? Math.random().toFixed(0) === '1' : true
+// const randomMockPosition = (ticker: string) => {
+//   const hasPut = Math.random().toFixed(0) === '1'
+//   const hasCall = hasPut ? Math.random().toFixed(0) === '1' : true
 
-  let maxLoss = 0
-  if (hasPut) {
-    maxLoss += Number(((15 + (Math.random() * 10)) * -1).toFixed(0))
-  }
-  if (hasCall) {
-    maxLoss += Number(((15 + (Math.random() * 10)) * -1).toFixed(0))
-  }
+//   let maxLoss = 0
+//   if (hasPut) {
+//     maxLoss += Number(((15 + (Math.random() * 10)) * -1).toFixed(0))
+//   }
+//   if (hasCall) {
+//     maxLoss += Number(((15 + (Math.random() * 10)) * -1).toFixed(0))
+//   }
 
-  const maxGain = 100 + maxLoss
-  const gainLoss = Number((maxLoss + (Math.random() * 100)).toFixed(0))
+//   const maxGain = 100 + maxLoss
+//   const gainLoss = Number((maxLoss + (Math.random() * 100)).toFixed(0))
 
-  return generateMockPosition(ticker, gainLoss, maxLoss, maxGain, hasPut, hasCall)
-}
+//   return generateMockPosition(ticker, gainLoss, maxLoss, maxGain, hasPut, hasCall)
+// }
 
-const symbols = [
-  'DIA', 'AAPL', 'TSLA', 'MSFT', 'BAC', 'WFC', 'FB', 'PTON', 'AMC', 'F',
-  'SNDL', 'AMZN', 'DIS', 'NIO', 'LCID', 'NFLX', 'PFE', 'NVDA', 'AAL', 'SNAP', 'PLUG', 'HOOD',
-  'GPRO', 'BABA', 'CCL', 'ACB', 'NOK', 'DAL', 'UAL', 'PLTR', 'GME', 'SBUX', 'AMD',
-  'COIN', 'TLRY', 'TWTR', 'RIVN', 'T', 'KO', 'CGC', 'GOOG', 'MRNA', 'SPCE', 'BB', 'PYPL', 'UBER',
-  'GM', 'ZNGA', 'NCLH', 'WKHS', 'SQ', 'DKNG', 'ABNB', 'BA', 'WMT',
-  'JNJ', 'CHPT', 'LUV', 'MRO', 'ARKK', 'RIOT', 'XOM', 'SOFI', 'WISH', 'SONY',
-  'PENN', 'COST', 'ZM', 'JPM',
-  'RCL', 'CLOV', 'ET', 'INTC', 'V', 'TSM', 'FUBO', 'MA',
-  'XLB', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLU', 'XLV', 'XLY',
+// const symbols = [
+//   'DIA', 'AAPL', 'TSLA', 'MSFT', 'BAC', 'WFC', 'FB', 'PTON', 'AMC', 'F',
+//   'SNDL', 'AMZN', 'DIS', 'NIO', 'LCID', 'NFLX', 'PFE', 'NVDA', 'AAL', 'SNAP', 'PLUG', 'HOOD',
+//   'GPRO', 'BABA', 'CCL', 'ACB', 'NOK', 'DAL', 'UAL', 'PLTR', 'GME', 'SBUX', 'AMD',
+//   'COIN', 'TLRY', 'TWTR', 'RIVN', 'T', 'KO', 'CGC', 'GOOG', 'MRNA', 'SPCE', 'BB', 'PYPL', 'UBER',
+//   'GM', 'ZNGA', 'NCLH', 'WKHS', 'SQ', 'DKNG', 'ABNB', 'BA', 'WMT',
+//   'JNJ', 'CHPT', 'LUV', 'MRO', 'ARKK', 'RIOT', 'XOM', 'SOFI', 'WISH', 'SONY',
+//   'PENN', 'COST', 'ZM', 'JPM',
+//   'RCL', 'CLOV', 'ET', 'INTC', 'V', 'TSM', 'FUBO', 'MA',
+//   'XLB', 'XLC', 'XLE', 'XLF', 'XLI', 'XLK', 'XLP', 'XLU', 'XLV', 'XLY',
 
-  'SPY', 'IWM', 'QQQ',
+//   'SPY', 'IWM', 'QQQ',
 
-  // Leveraged
-  'TQQQ', 'SOXL', 'DUST', 'ERX', 'FAS', 'FAZ', 'JNUG', 'LABD', 'LABU', 'NUGT', 'SDS', 'TNA', 'UPRO', 'YINN'
-]
+//   // Leveraged
+//   'TQQQ', 'SOXL', 'DUST', 'ERX', 'FAS', 'FAZ', 'JNUG', 'LABD', 'LABU', 'NUGT', 'SDS', 'TNA', 'UPRO', 'YINN'
+// ]
 
-const mockPositions = symbols.map(symbol => randomMockPosition(symbol))
+// const mockPositions = symbols.map(symbol => randomMockPosition(symbol))
 
-mockPositions.push(generateMockPosition('FAKE', 40000, -20, 35, true, true))
-mockPositions.push(generateMockPosition('FAKE2', -50000, -20, 50, true, true))
+// mockPositions.push(generateMockPosition('FAKE', 40000, -20, 35, true, true))
+// mockPositions.push(generateMockPosition('FAKE2', -50000, -20, 50, true, true))
 
 //const mockEquity = 20000 + (Math.random() * 40000)
 //const mockMonthEarnings = -500 + (Math.random() * 1000)
@@ -85,10 +85,6 @@ const ShowcaseController = () => {
   const [ theft, setTheft ] = useState<number>(0)
   const [ lastYearTheft, setLastYearTheft ] = useState<number>(0)
   const [ positions, setPositions ] = useState<PositionChitProps[]>([])
-
-  // TODO Network call
-
-  console.log(positions)
 
   // Refresh every 15 minutes
   useEffect(() => {
@@ -131,7 +127,7 @@ const ShowcaseController = () => {
       yearEarnings={yearEarnings}
       theft={theft}
       lastYearTheft={lastYearTheft}
-      positions={mockPositions}
+      positions={positions}
     />
   )
 }
