05bf4e3330b1cf24acb4da778f252b08486a3b75
Mike <mweinberg21@gmail.com>
2022-04-04 00:28:23 -0500

    Little more tradier refactoring

--- packages/tradier/src/getBalances.ts
+++ packages/tradier/src/getBalances.ts
@@ -1,12 +1,9 @@
-import * as network from './network'
+import { callTradierHelper } from './callTradierHelper'
 
 // TODO Fill this out
 export type Balances = {
   total_equity: number
 }
 
-export const getBalances = async () : Promise<Balances> => {
-  const url = `accounts/${process.env.ACCOUNTNUM}/balances`
-  const response = await network.get(url)
-  return response.balances
-}
+export const getBalances = async () : Promise<Balances> =>
+  callTradierHelper(`accounts/${process.env.ACCOUNTNUM}/balances`, 'balances', null, false)
--- packages/tradier/src/getGainLoss.ts
+++ packages/tradier/src/getGainLoss.ts
@@ -16,7 +16,11 @@ export const getGainLoss = async (
   limit: number = 1000,
   start: string = '2020-01-01',
   end: string = new Date().toISOString().split('T')[0],
-) : Promise<GainLossDetails[]> => {
-  const url = `accounts/${process.env.ACCOUNTNUM}/gainloss?page=${pageNum}&limit=${limit}&start=${start}&end=${end}`
-  return callTradierHelper(url, 'gainloss', 'closed_position', true)
-}
+) : Promise<GainLossDetails[]> =>
+  callTradierHelper(
+    `accounts/${process.env.ACCOUNTNUM}/gainloss?page=${pageNum}&limit=${limit}&start=${start}&end=${end}`,
+    'gainloss',
+    'closed_position',
+    true
+  )
+
