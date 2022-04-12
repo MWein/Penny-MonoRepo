2dcb63b82b7173a03dda0344b1c10fb24dce27a9
Mike <mweinberg21@gmail.com>
2022-04-11 20:44:40 -0500

    Wrote most of function and some tests

--- packages/spreadOutcome/index.test.ts
+++ packages/spreadOutcome/index.test.ts
@@ -1,8 +1,8 @@
 import { getSpreadOutcome } from './index'
-import { mockPo } from '@penny/test-helpers'
+import { generatePositionObject } from '@penny/test-helpers'
 
 describe('getSpreadOutcome', () => {
-  it.only('Returns all 0 if given empty array', () => {
+  it('Returns all 0 if given empty array', () => {
     expect(getSpreadOutcome('AAPL', [])).toEqual({
       ticker: 'AAPL',
       side: 'indeterminate',
@@ -12,48 +12,138 @@ describe('getSpreadOutcome', () => {
     })
   })
 
-  it.only('Side is short for net-sold spread', () => {
-
+  it('Side is short for net-sold spread', () => {
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 14, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', -1, 'call', -52, '2021-01-01', 1234, '2021-01-01', 100)
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result.side).toEqual('short')
   })
 
-  it.only('Side is long for net-bought spread', () => {
-
+  it('Side is long for net-bought spread', () => {
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 14, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', -1, 'call', -2, '2021-01-01', 1234, '2021-01-01', 100)
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result.side).toEqual('long')
   })
 
   it('Fully covered should be true if positions all have opposites', () => {
-
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 14, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', -1, 'call', -2, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', 1, 'put', 14, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', -1, 'put', -2, '2021-01-01', 1234, '2021-01-01', 100),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result.fullyCovered).toEqual(true)
   })
 
   it('Fully covered should be false if positions dont all have opposites', () => {
-    
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 14, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', -1, 'call', -2, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', 2, 'put', 14, '2021-01-01', 1234, '2021-01-01', 100),
+      generatePositionObject('AAPL', -1, 'put', -2, '2021-01-01', 1234, '2021-01-01', 100),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result.fullyCovered).toEqual(false)
   })
 
   it('Max gain and loss correct for net bought call spread', () => {
-
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 52, '2021-01-01', 1234, '2021-01-01', 170),
+      generatePositionObject('AAPL', -1, 'call', -20, '2021-01-01', 1234, '2021-01-01', 172.5),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'long',
+      maxLoss: -32,
+      maxGain: 218,
+      fullyCovered: true,
+    })
   })
 
   it('Max gain and loss correct for net bought put spread', () => {
-    
+    const positions = [
+      generatePositionObject('AAPL', 1, 'put', 103, '2021-01-01', 1234, '2021-01-01', 162.5),
+      generatePositionObject('AAPL', -1, 'put', -58, '2021-01-01', 1234, '2021-01-01', 160),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'long',
+      maxLoss: -45,
+      maxGain: 205,
+      fullyCovered: true,
+    })
   })
 
   it('Max gain and loss correct for net sold call spread', () => {
-
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 8, '2021-01-01', 1234, '2021-01-01', 175),
+      generatePositionObject('AAPL', -1, 'call', -20, '2021-01-01', 1234, '2021-01-01', 172.5),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'short',
+      maxLoss: -238,
+      maxGain: 12,
+      fullyCovered: true,
+    })
   })
 
   it('Max gain and loss correct for net sold put spread', () => {
-    
-  })
-
-  it('Max gain and loss correct for net bought call spread', () => {
-
+    const positions = [
+      generatePositionObject('AAPL', 1, 'put', 58, '2021-01-01', 1234, '2021-01-01', 160),
+      generatePositionObject('AAPL', -1, 'put', -103, '2021-01-01', 1234, '2021-01-01', 162.5),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'short',
+      maxLoss: -205,
+      maxGain: 45,
+      fullyCovered: true,
+    })
   })
 
   it('Max gain and loss correct for net bought iron condor', () => {
-    
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 52, '2021-01-01', 1234, '2021-01-01', 170),
+      generatePositionObject('AAPL', -1, 'call', -20, '2021-01-01', 1234, '2021-01-01', 172.5),
+      generatePositionObject('AAPL', 1, 'put', 103, '2021-01-01', 1234, '2021-01-01', 162.5),
+      generatePositionObject('AAPL', -1, 'put', -58, '2021-01-01', 1234, '2021-01-01', 160),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'long',
+      maxLoss: -77,
+      maxGain: 173,
+      fullyCovered: true,
+    })
   })
 
   it('Max gain and loss correct for net sold iron condor', () => {
-    
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 8, '2021-01-01', 1234, '2021-01-01', 175),
+      generatePositionObject('AAPL', -1, 'call', -20, '2021-01-01', 1234, '2021-01-01', 172.5),
+      generatePositionObject('AAPL', -1, 'put', -103, '2021-01-01', 1234, '2021-01-01', 162.5),
+      generatePositionObject('AAPL', 1, 'put', 58, '2021-01-01', 1234, '2021-01-01', 160),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'short',
+      maxLoss: -193,
+      maxGain: 57,
+      fullyCovered: true,
+    })
   })
 
   it('Max gain should be Infinity and max loss the cost basis for long straddle', () => {
--- packages/spreadOutcome/index.ts
+++ packages/spreadOutcome/index.ts
@@ -1,5 +1,5 @@
 import { Position } from "@penny/tradier"
-import { getUnderlying, getType, getStrike } from "@penny/option-symbol-parser" 
+import { getUnderlying, getType, getStrike, isOption } from "@penny/option-symbol-parser" 
 
 
 type SpreadSide = 'long' | 'short' | 'indeterminate'
@@ -10,6 +10,11 @@ export type SpreadOutcome = {
   maxLoss: number,
   maxGain: number,
   fullyCovered: boolean,
+}
+
+type PositionResult = {
+  invoked: boolean,
+  value: number,
 }
 
 
@@ -23,20 +28,85 @@ const determineSide = (costBasis: number): SpreadSide => {
 }
 
 
+const determinePositionResultForStrike = (position: Position, strike: number): PositionResult => {
+  const posType = getType(position.symbol)
+  const posStrike = getStrike(position.symbol)
+
+  // const invoked = posType === 'call' ? strike >= posStrike : strike <= posStrike
+  // const value = (posType === 'call' ? posStrike * -100 : posStrike * 100) * position.quantity
+  // const returnValue = invoked ? value : 0
+
+  // return {
+  //   invoked,
+  //   value: returnValue,
+  // }
+
+  // TODO Refactor
+  if (posType === 'call') {
+    if (strike >= posStrike) {
+      const purchaseValue = posStrike * -100
+      return {
+        invoked: true,
+        value: purchaseValue * position.quantity
+      }
+    }
+    return {
+      invoked: false,
+      value: 0,
+    }
+  }
+  
+  // Put
+  if (strike <= posStrike) {
+    const saleValue = posStrike * 100
+    return {
+      invoked: true,
+      value: saleValue * position.quantity
+    }
+  }
+  return {
+    invoked: false,
+    value: 0,
+  }
+}
+
+
 // Assumes that all of them expire on the same day
 export const getSpreadOutcome = (underlying: string, positions: Position[]): SpreadOutcome => {
-  const onlyUnderlying = positions.filter(pos => getUnderlying(pos.symbol) === underlying)
+  const onlyUnderlying = positions.filter(pos => getUnderlying(pos.symbol) === underlying && isOption(pos.symbol))
   const costBasis = positions.reduce((acc, pos) => acc + pos.cost_basis, 0)
 
   const strikes = onlyUnderlying.map(pos => getStrike(pos.symbol)).sort((a, b) => a - b)
 
-  console.log(strikes)
+  strikes.push(strikes[strikes.length - 1] + 1) // Add last strike plus 1
+  strikes.unshift(strikes[0] - 1) // Add first strike minus 1
+  
+  const resultAtEachStrike = strikes.map(strike =>
+    onlyUnderlying.reduce((acc, pos) => {
+      const { invoked, value } = determinePositionResultForStrike(pos, strike)
+      return {
+        numInvoked: acc.numInvoked + (invoked ? 1 : 0),
+        total: acc.total + value
+      }
+    }, {
+      numInvoked: 0,
+      total: 0,
+    })
+  ).filter(result => result.numInvoked % 2 === 0).map(result => result.total - costBasis)
+
+  // For iron condors, there would be no strike between the spreads with the above, adding it manually
+  // A lot less complex
+  // Also theres a weird edge case where -0 is returned, using a ternary to just return 0
+  resultAtEachStrike.push(costBasis === 0 ? 0 : costBasis * -1)
+
+  const maxLoss = Math.min(...resultAtEachStrike)
+  const maxGain = Math.max(...resultAtEachStrike)
 
   return {
     ticker: underlying,
     side: determineSide(costBasis),
-    maxLoss: 0,
-    maxGain: 0,
+    maxLoss,
+    maxGain,
     fullyCovered: onlyUnderlying.reduce((acc, pos) => acc + pos.quantity, 0) === 0,
   }
 }
