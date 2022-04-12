d5191b22435e8689a0297b447e19566f6f7ba806
Mike <mweinberg21@gmail.com>
2022-04-11 21:26:54 -0500

    Finished tests

--- packages/spreadOutcome/index.test.ts
+++ packages/spreadOutcome/index.test.ts
@@ -52,6 +52,9 @@ describe('getSpreadOutcome', () => {
     expect(result.fullyCovered).toEqual(false)
   })
 
+
+  // ********** Call and Put Spreads **********
+
   it('Max gain and loss correct for net bought call spread', () => {
     const positions = [
       generatePositionObject('AAPL', 1, 'call', 52, '2021-01-01', 1234, '2021-01-01', 170),
@@ -112,6 +115,9 @@ describe('getSpreadOutcome', () => {
     })
   })
 
+
+  // ********** Iron Condors **********
+
   it('Max gain and loss correct for net bought iron condor', () => {
     const positions = [
       generatePositionObject('AAPL', 1, 'call', 52, '2021-01-01', 1234, '2021-01-01', 170),
@@ -146,11 +152,141 @@ describe('getSpreadOutcome', () => {
     })
   })
 
+  it('Max gain and loss correct for uneven net bought iron condor', () => {
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 52, '2021-01-01', 1234, '2021-01-01', 170),
+      generatePositionObject('AAPL', -1, 'call', -20, '2021-01-01', 1234, '2021-01-01', 172.5),
+      generatePositionObject('AAPL', 1, 'put', 103, '2021-01-01', 1234, '2021-01-01', 162.5),
+      generatePositionObject('AAPL', -1, 'put', -34, '2021-01-01', 1234, '2021-01-01', 157.5),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'long',
+      maxLoss: -101,
+      maxGain: 399,
+      fullyCovered: true,
+    })
+  })
+
+  it('Max gain and loss correct for uneven net sold iron condor', () => {
+    const positions = [
+      generatePositionObject('AAPL', -1, 'call', -52, '2021-01-01', 1234, '2021-01-01', 170),
+      generatePositionObject('AAPL', 1, 'call', 20, '2021-01-01', 1234, '2021-01-01', 172.5),
+      
+      generatePositionObject('AAPL', -1, 'put', -103, '2021-01-01', 1234, '2021-01-01', 162.5),
+      generatePositionObject('AAPL', 1, 'put', 34, '2021-01-01', 1234, '2021-01-01', 157.5),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'short',
+      maxLoss: -399,
+      maxGain: 101,
+      fullyCovered: true,
+    })
+  })
+
+
+
+  // ********** Iron Butterflies **********
+
+  it('Max gain and loss correct for net bought iron butterfly', () => {
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 259, '2021-01-01', 1234, '2021-01-01', 165),
+      generatePositionObject('AAPL', -1, 'call', -20, '2021-01-01', 1234, '2021-01-01', 172.5),
+      
+      generatePositionObject('AAPL', 1, 'put', 180, '2021-01-01', 1234, '2021-01-01', 165),
+      generatePositionObject('AAPL', -1, 'put', -58, '2021-01-01', 1234, '2021-01-01', 160),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'long',
+      maxLoss: -361,
+      maxGain: 389,
+      fullyCovered: true,
+    })
+  })
+
+  it('Max gain and loss correct for net sold iron butterfly', () => {
+    const positions = [
+      generatePositionObject('AAPL', -1, 'call', -259, '2021-01-01', 1234, '2021-01-01', 165),
+      generatePositionObject('AAPL', 1, 'call', 20, '2021-01-01', 1234, '2021-01-01', 172.5),
+      
+      generatePositionObject('AAPL', -1, 'put', -180, '2021-01-01', 1234, '2021-01-01', 165),
+      generatePositionObject('AAPL', 1, 'put', 58, '2021-01-01', 1234, '2021-01-01', 160),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'short',
+      maxLoss: -389,
+      maxGain: 361,
+      fullyCovered: true,
+    })
+  })
+
+
+  // ********** Straddles and Strangles **********
+
   it('Max gain should be Infinity and max loss the cost basis for long straddle', () => {
-    
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 259, '2021-01-01', 1234, '2021-01-01', 165),
+      generatePositionObject('AAPL', 1, 'put', 180, '2021-01-01', 1234, '2021-01-01', 165),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'long',
+      maxLoss: -439,
+      maxGain: Infinity,
+      fullyCovered: false,
+    })
   })
 
   it('Max loss should be Infinity and max gain the cost basis for short straddle', () => {
-    
+    const positions = [
+      generatePositionObject('AAPL', -1, 'call', -259, '2021-01-01', 1234, '2021-01-01', 165),
+      generatePositionObject('AAPL', -1, 'put', -180, '2021-01-01', 1234, '2021-01-01', 165),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'short',
+      maxLoss: -Infinity,
+      maxGain: 439,
+      fullyCovered: false,
+    })
+  })
+
+  it('Max gain should be Infinity and max loss the cost basis for long strangle', () => {
+    const positions = [
+      generatePositionObject('AAPL', 1, 'call', 129, '2021-01-01', 1234, '2021-01-01', 167.5),
+      generatePositionObject('AAPL', 1, 'put', 180, '2021-01-01', 1234, '2021-01-01', 165),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'long',
+      maxLoss: -309,
+      maxGain: Infinity,
+      fullyCovered: false,
+    })
+  })
+
+  it('Max loss should be Infinity and max gain the cost basis for short straddle', () => {
+    const positions = [
+      generatePositionObject('AAPL', -1, 'call', -129, '2021-01-01', 1234, '2021-01-01', 167.5),
+      generatePositionObject('AAPL', -1, 'put', -180, '2021-01-01', 1234, '2021-01-01', 165),
+    ]
+    const result = getSpreadOutcome('AAPL', positions)
+    expect(result).toEqual({
+      ticker: 'AAPL',
+      side: 'short',
+      maxLoss: -Infinity,
+      maxGain: 309,
+      fullyCovered: false,
+    })
   })
 })
