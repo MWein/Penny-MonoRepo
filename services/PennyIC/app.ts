f4df45ece132ab7ab2bd8d23cea446f0d9585568
Mike <mweinberg21@gmail.com>
2022-04-13 20:41:41 -0500

    Cron wrapper function

--- services/PennyIC/app.ts
+++ services/PennyIC/app.ts
@@ -1,7 +1,7 @@
 const CronJob = require('cron').CronJob
 const mongoose = require('mongoose')
 
-import { log, logCron, clearOldLogs } from '@penny/logger'
+import { log, logCron, clearOldLogs, CronType } from '@penny/logger'
 
 // import {
 //   sellIronCondors
@@ -14,43 +14,12 @@ import {
 } from './core/closeExpiringPositions'
 
 
-
-const openIronCondorsCron = async () => {
+const cronFunc = async (func: Function, cronName: CronType) => {
   try {
-    await buyIronCondors()
-    logCron('OpenICs', true)
+    await func()
+    logCron(cronName, true)
   } catch (e) {
-    log({
-      type: 'error',
-      message: e.toString()
-    })
-    logCron('OpenICs', false, e.toString())
-  }
-}
-
-const closeExpiringPositionsCron = async () => {
-  try {
-    await closeExpiringPositions()
-    logCron('CloseExp', true)
-  } catch (e) {
-    log({
-      type: 'error',
-      message: e.toString()
-    })
-    logCron('CloseExp', false, e.toString())
-  }
-}
-
-const housekeepingCron = async () => {
-  try {
-    await clearOldLogs()
-    logCron('Housekeeping', true)
-  } catch (e) {
-    log({
-      type: 'error',
-      message: e.toString()
-    })
-    logCron('Housekeeping', false, e.toString())
+    logCron(cronName, false, e.toString())
   }
 }
 
@@ -71,15 +40,15 @@ const launchCrons = async () => {
   // }, null, true, 'America/New_York')
 
 
-  new CronJob('0 0 10 * * 1-4', openIronCondorsCron, null, true, 'America/New_York')
-  new CronJob('0 0 13 * * 1-4', openIronCondorsCron, null, true, 'America/New_York')
+  new CronJob('0 0 10 * * 1-4', () => cronFunc(buyIronCondors, 'OpenICs'), null, true, 'America/New_York')
+  new CronJob('0 0 13 * * 1-4', () => cronFunc(buyIronCondors, 'OpenICs'), null, true, 'America/New_York')
 
   // One hour before Tradier does it
-  new CronJob('0 15 14 * * 1-5', closeExpiringPositionsCron, null, true, 'America/New_York')
+  new CronJob('0 15 14 * * 1-5', () => cronFunc(closeExpiringPositions, 'CloseExp'), null, true, 'America/New_York')
 
   // Run every day at 4:10 NY time
   // 10 mins after market close
-  new CronJob('0 10 16 * * *', housekeepingCron, null, true, 'America/New_York')
+  new CronJob('0 10 16 * * *', () => cronFunc(clearOldLogs, 'Housekeeping'), null, true, 'America/New_York')
 
   console.log('Deployment successful')
 }
