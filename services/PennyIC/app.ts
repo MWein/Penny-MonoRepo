1daee739df206296911b0dab6202933de29efbfa
Mike <mweinberg21@gmail.com>
2022-04-14 10:31:06 -0500

    Crons werent working

--- services/PennyIC/app.ts
+++ services/PennyIC/app.ts
@@ -16,14 +16,12 @@ import {
 
 
 const cronFunc = async (func: Function, cronName: CronType) => {
-  return async () => {
     try {
       await func()
       logCron(cronName, true)
     } catch (e) {
       logCron(cronName, false, e.toString())
     }
-  }
 }
 
 
@@ -43,15 +41,15 @@ const launchCrons = async () => {
   // }, null, true, 'America/New_York')
 
 
-  new CronJob('0 0 10 * * 1-4', cronFunc(buyIronCondors, 'OpenICs'), null, true, 'America/New_York')
-  new CronJob('0 0 13 * * 1-4', cronFunc(buyIronCondors, 'OpenICs'), null, true, 'America/New_York')
+  new CronJob('0 0 10 * * 1-4', () => cronFunc(buyIronCondors, 'OpenICs'), null, true, 'America/New_York')
+  new CronJob('0 0 13 * * 1-4', () => cronFunc(buyIronCondors, 'OpenICs'), null, true, 'America/New_York')
 
   // One hour before Tradier does it
-  new CronJob('0 15 14 * * 1-5', cronFunc(closeExpiringPositions, 'CloseExp'), null, true, 'America/New_York')
+  new CronJob('0 15 14 * * 1-5', () => cronFunc(closeExpiringPositions, 'CloseExp'), null, true, 'America/New_York')
 
   // Run every day at 4:10 NY time
   // 10 mins after market close
-  new CronJob('0 10 16 * * *', cronFunc(clearOldLogs, 'Housekeeping'), null, true, 'America/New_York')
+  new CronJob('0 10 16 * * *', () => cronFunc(clearOldLogs, 'Housekeeping'), null, true, 'America/New_York')
 
   // For deploy script checking
   console.log('Deployment successful')
