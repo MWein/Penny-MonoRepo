59834e824c7b0c3b92a3a5fc567a5f6c02683d19
Mike <mweinberg21@gmail.com>
2022-04-24 13:32:46 -0500

    Removed torture test

--- services/PennyIC/common/pickRandomTickers.ts
+++ services/PennyIC/common/pickRandomTickers.ts
@@ -19,7 +19,6 @@ const pickRandomTickersHelper = async (
   const newCandidates = candidates.filter(x => x !== candidate)
 
   if (evaluation.valid) {
-    console.log('SELECTION', candidate)
     const newSelectedTickers = [ ...selectedTickers, candidate ]
     if (newSelectedTickers.length === sellPerDay) {
       return newSelectedTickers
@@ -27,7 +26,6 @@ const pickRandomTickersHelper = async (
     return pickRandomTickersHelper(newCandidates, sellPerDay, maxStrikeWidth, expiration, newSelectedTickers)
   }
 
-  console.log('FAILURE', candidate)
   return pickRandomTickersHelper(newCandidates, sellPerDay, maxStrikeWidth, expiration, selectedTickers)  
 }
 
