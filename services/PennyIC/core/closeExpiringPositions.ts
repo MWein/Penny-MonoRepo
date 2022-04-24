7656d1fa5d9d9ebe41a8d50caaf9076a209bde56
Mike <mweinberg21@gmail.com>
2022-04-23 23:50:54 -0500

    Removed logs for closeExpiringPositions

--- services/PennyIC/core/closeExpiringPositions.ts
+++ services/PennyIC/core/closeExpiringPositions.ts
@@ -1,12 +1,9 @@
 import * as tradier from '@penny/tradier'
-import * as logger from '@penny/logger'
 import { getExpiration, isOption } from '@penny/option-symbol-parser'
 import { closePositions } from '../common/closePositions'
 
 
 export const closeExpiringPositions = async () => {
-  logger.log('Closing expiring positions')
-
   const positions = await tradier.getPositions()
   const optionPositions = positions.filter(pos => isOption(pos.symbol))
 
@@ -14,6 +11,4 @@ export const closeExpiringPositions = async () => {
   const expiringToday = optionPositions.filter(pos => getExpiration(pos.symbol) === today)
 
   await closePositions(expiringToday)
-
-  logger.log('Finished closing expiring positions')
 }
