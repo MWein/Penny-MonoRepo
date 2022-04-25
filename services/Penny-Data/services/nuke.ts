150e6c788c1be32714bfed69601c3b2deea0108b
Mike <mweinberg21@gmail.com>
2022-04-24 23:07:22 -0500

    nuke tests

--- services/Penny-Data/services/nuke.ts
+++ services/Penny-Data/services/nuke.ts
@@ -1,15 +1,14 @@
 import * as tradier from '@penny/tradier'
-import { getSpreadOutcomes, SpreadOutcome, SpreadSide } from '@penny/spread-outcome'
+import { getSpreadOutcomes, SpreadOutcome } from '@penny/spread-outcome'
 import { isOption } from '@penny/option-symbol-parser'
 
 type SidesToClose = 'long' | 'short' | 'all'
-
 
 const getSpreadsToClose = (spreads: SpreadOutcome[], type: SidesToClose) => {
   if (type === 'long') {
     return spreads.filter(spread => spread.side === 'long')
   } else if (type === 'short') {
-    return spreads.filter(spread => spread.side === 'long')
+    return spreads.filter(spread => spread.side === 'short')
   }
   return spreads
 }
@@ -18,6 +17,10 @@ const getSpreadsToClose = (spreads: SpreadOutcome[], type: SidesToClose) => {
 export const nuke = async (sideToClose: SidesToClose) => {
   const positions = await tradier.getPositions()
   const optionPositions = positions.filter(pos => isOption(pos.symbol))
+
+  if (optionPositions.length === 0) {
+    return
+  }
 
   const spreads = getSpreadOutcomes(optionPositions)
   const spreadsToClose = getSpreadsToClose(spreads, sideToClose)
