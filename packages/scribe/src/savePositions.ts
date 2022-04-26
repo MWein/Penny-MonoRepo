afe1a2bca00a0d0c2324d426eb10fcd1f2e5bf18
Mike <mweinberg21@gmail.com>
2022-04-25 23:18:51 -0500

    savePositions test

--- packages/scribe/src/savePositions.ts
+++ packages/scribe/src/savePositions.ts
@@ -8,6 +8,11 @@ import { positionSnapshotModel } from '@penny/db-models'
 export const savePositions = async () => {
   const positions = await tradier.getPositions()
   const optionPositions = positions.filter(pos => isOption(pos.symbol))
+
+  if (optionPositions.length === 0) {
+    return
+  }
+
   const existingModels = await positionSnapshotModel.find()
   const existingSymbols = existingModels.map(model => model.symbol)
   const newPositions = optionPositions.filter(pos => !existingSymbols.includes(pos.symbol))
