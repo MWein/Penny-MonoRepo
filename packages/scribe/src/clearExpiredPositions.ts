8c1939e56b94285df6f7b9d488ce012b6c78c748
Mike <mweinberg21@gmail.com>
2022-04-25 22:22:49 -0500

    Clear expiring positions function

---
+++ packages/scribe/src/clearExpiredPositions.ts
@@ -0,0 +1,16 @@
+import * as tradier from '@penny/tradier'
+import { positionSnapshotModel } from '@penny/db-models'
+import { getExpiration } from '@penny/option-symbol-parser'
+
+// This function clears out any positions that expired
+// By option expiration date
+
+export const clearExpiredPositions = async () => {
+  const today = new Date()
+  const existingModels = await positionSnapshotModel.find()
+  const idsToDelete = existingModels.reduce((acc, pos) => {
+    const expiration = new Date(getExpiration(pos.symbol))
+    return today > expiration ? [ ...acc, pos._id ] : acc
+  }, [])
+  await positionSnapshotModel.deleteMany({ _id: { $in: idsToDelete } })
+}
--- packages/scribe/src/index.ts
+++ packages/scribe/src/index.ts
@@ -1,1 +1,2 @@
 export * from './savePositions'
+export * from './clearExpiredPositions'
