e56c20219b51dbfe7965637299bbca6d503f8bcb
Mike <mweinberg21@gmail.com>
2022-07-17 01:04:53 -0500

    Filtering for open positions only for efficiency

--- packages/tastyCSVParser/src/containsExpiredPositions.ts
+++ packages/tastyCSVParser/src/containsExpiredPositions.ts
@@ -3,6 +3,7 @@ import { TastyPosition } from './groupPositions'
 export const containsExpiredPositions = (positions: TastyPosition[]) => {
   const today = new Date().toISOString().split('T')[0]
   return positions
+    .filter(pos => pos.state === 'Open')
     .flatMap(
       pos => pos.legs
         .filter(leg => leg.state === 'Open'))
