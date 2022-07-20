e7948550db58e41134af6c29cc112ae64a222154
Mike <mweinberg21@gmail.com>
2022-07-19 23:46:03 -0500

    Wrapped in a try-catch

--- services/Penny-Data/services/saveNewTransactions.ts
+++ services/Penny-Data/services/saveNewTransactions.ts
@@ -10,37 +10,44 @@ type SaveTransactionsResult = {
 
 
 export const saveNewTransactions = async (csvData: string): Promise<SaveTransactionsResult> => {
-  if (!tastyCSVParser.verify(csvData)) {
-    return {
-      error: true,
-      message: 'Invalid CSV'
+  try {
+    if (!tastyCSVParser.verify(csvData)) {
+      return {
+        error: true,
+        message: 'Invalid CSV'
+      }
     }
-  }
-
-  const history = tastyCSVParser.historyCSVToJson(csvData)
-    .map(hist => ({
-      ...hist,
-      hash: createHash('sha256').update(JSON.stringify(hist)).digest('hex')
-    }))
-
-  const hashes = history.map(hist => hist.hash)
-  const existingHashesRes = await tastyHistoryModel
-    .find({ hash: { $in: hashes } }).select('hash').lean()
-  const existingHashes = existingHashesRes.map(res => res.hash)
-
-  const newHistory = history.filter(hist => !existingHashes.includes(hist.hash))
-
-  if (newHistory.length === 0) {
+  
+    const history = tastyCSVParser.historyCSVToJson(csvData)
+      .map(hist => ({
+        ...hist,
+        hash: createHash('sha256').update(JSON.stringify(hist)).digest('hex')
+      }))
+  
+    const hashes = history.map(hist => hist.hash)
+    const existingHashesRes = await tastyHistoryModel
+      .find({ hash: { $in: hashes } }).select('hash').lean()
+    const existingHashes = existingHashesRes.map(res => res.hash)
+  
+    const newHistory = history.filter(hist => !existingHashes.includes(hist.hash))
+  
+    if (newHistory.length === 0) {
+      return {
+        error: false,
+        message: 'No new records added'
+      }
+    }
+  
+    await tastyHistoryModel.insertMany(newHistory)
+  
     return {
       error: false,
-      message: 'No new records added'
+      message: `Added ${newHistory.length} new record${newHistory.length === 1 ? '' : 's'}`
     }
-  }
-
-  await tastyHistoryModel.insertMany(newHistory)
-
-  return {
-    error: false,
-    message: `Added ${newHistory.length} new record${newHistory.length === 1 ? '' : 's'}`
+  } catch (e) {
+    return {
+      error: true,
+      message: 'Error processing CSV'
+    }
   }
 }
